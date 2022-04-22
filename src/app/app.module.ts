8cc677fdc2a9fe563a08b4a1f1fc8079e9c754f8
pengpeng <501858681@qq.com>
2022-04-22 13:51:43 +0800

    the project is too old to work, so I delete it and try to refactor the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,67 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RecentTacosComponent } from './recents/recents.component';
-import { ApiService } from './api/ApiService';
-import { RecentTacosService } from './recents/RecentTacosService';
-import { SpecialsComponent } from './specials/specials.component';
-
-import { routes } from './app.routes';
-import { CloudTitleComponent } from './cloud-title/cloudtitle.component';
-import { NonWrapsPipe } from './recents/NonWrapsPipe';
-import { WrapsPipe } from './recents/WrapsPipe';
-import { DesignComponent } from './design/design.component';
-import { GroupBoxComponent } from './group-box/groupbox.component';
-import { BigButtonComponent } from './big-button/bigbutton.component';
-import { LittleButtonComponent } from './little-button/littlebutton.component';
-import { LocationsComponent } from './locations/locations.component';
-import { FormGroupDirective } from '@angular/forms/src/directives/reactive_directives/form_group_directive';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { OAuthModule } from 'angular-oauth2-oidc';
-import { CartComponent } from './cart/cart.component';
-import { CartService } from './cart/cart-service';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    LoginComponent,
-    FooterComponent,
-    RecentTacosComponent,
-    SpecialsComponent,
-    LocationsComponent,
-    CloudTitleComponent,
-    DesignComponent,
-    CartComponent,
-    NonWrapsPipe,
-    WrapsPipe,
-    GroupBoxComponent,
-    BigButtonComponent,
-    LittleButtonComponent,
-  ],
   imports: [
     BrowserModule,
-    HttpModule,
     HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(routes),
-    OAuthModule.forRoot()
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ]),
   ],
-  providers: [
-    ApiService,
-    CartService,
-    RecentTacosService,
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
